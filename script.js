8c1b760498f2632e55cf2c75b82dfa5c146e73f8
Alkeo123123 <alkeocepani7@gmail.com>
2024-07-17 15:12:03 +0200

    fourth commit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const modeToggle = document.getElementById('mode-toggle');
-const body = document.body;
+const personi = {
+  currentBalance: "300"
+};
 
-modeToggle.addEventListener('click', () => body.classList.toggle('dark-mode'));
+const detyra = ({ currentBalance }, withdraw) => {
+  const balance = parseInt(currentBalance);
+  if (balance >= withdraw) {
+    console.log(`Ti ke para te mjaftueshme per te terhequr ${withdraw}. Balanca e re: ${balance - withdraw}`);
+  } else {
+    console.log(`Nuk ke para te mjaftueshme per kete transaksion, te duhen ${withdraw - balance} para per te perfunduar transaksionin.`);
+  }
+};
+
+detyra(personi, 200);
